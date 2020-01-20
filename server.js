c556c3ed964aa4a6a606dcac523b2e772f026a1c
Sazi <s_mtandabuzo@yahoo.com>
2020-01-20 21:00:36 +0200

    deploy

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const compression = require('compression');
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 8080;
 
-// Gzip
-app.use(compression());
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/prison-pwa'));
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-app.listen(port);
-
-// For all GET requests, send back index.html so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/prison-pwa/index.html'));
 });
 
-console.log(`Server listening on ${port}`);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
